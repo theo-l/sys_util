9d822a124dab996e47c714e6cc7e275c0ec8f1a3
theo-l <theol.liang@gamil.com>
2016-11-17 18:19:03 -0200

    complete arguments check

--- bin/uncompress.sh
+++ bin/uncompress.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
 # 用来进行解压系统中的压缩文件的通用方法
+#tar
+#命令参数
+#A: 向一个归档文件中追加一个 tar 文件
+#c: 创建归档文件
+#x: 解压归档文件
+#r: 想归档文件的末尾追加文件
+#t: 查看归档文件中的内容
+#u: 更新原归档文件中的文件
+
+#选项参数
+#z: gzip
+#j: bz2
+#Z:compress
+#v: 显示所有过程
+#O: 将文件解压缩到标准输出。
+
+
+
 
 
 # 检测使用的系统命令是否存在，否则安装该命令
@@ -17,7 +35,7 @@ __uncompress_file() {
 	archived_file=$1
 	dest_dir=$2
 
-	echo "$archived_file, $dest_dir"
+    # echo "$archived_file, $dest_dir"
 
 	# 解压 .zip 文件
 	if [[ $archived_file =~ .*.zip$ ]]; then
@@ -53,6 +71,9 @@ __uncompress_file() {
 		__detect_cmd unrar
 		unrar e $archived_file
 
+    else
+        echo -e "The file format not recognized"
+        exit 1
 	fi
 }
 
@@ -67,7 +88,7 @@ __parse_args() {
 	elif [[ $# == 1 ]]; then
 		archived_file=$1
 		dest_dir=./
-	elif [[ $# < 1 ]]; then
+	elif [[ $# < 1  || $# >2 ]]; then
 		echo -e "使用方法: $0  压缩文件名  解压缩文件路径名\n"
 		exit 1
 	fi
