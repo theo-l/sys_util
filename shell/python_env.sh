dc1a25136cbed748586006c7179195e8b6b62303
theo-l <theol.liang@gamil.com>
2016-11-28 22:53:11 -0200

    using python3

--- shell/python_env.sh
+++ shell/python_env.sh
@@ -8,4 +8,7 @@
 export PATH=$PATH:$SOFTWARE_HOME/pycharm/bin
 
 # 每次打开终端时进入到python2的虚拟环境中
-source ~/.virtualenv/py2/bin/activate
+#source ~/.virtualenv/py2/bin/activate
+
+# 每次打开终端时进入到python3的虚拟环境中
+source ~/.virtualenv/py3/bin/activate
