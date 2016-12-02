bc31449cb6fc9e08aa3e008ab3f6eb8825fb80fd
theo-l <theol.liang@gamil.com>
2016-12-02 13:20:21 -0200

    commit by schedule

--- shell/schedule_sys.py
+++ shell/schedule_sys.py
@@ -2,10 +2,10 @@
 # encoding:utf-8
 
 # 个人偏好系统调度器程序
+
 import os
 import schedule
 import time
-
 from git import Repo
 
 project_base = os.path.expanduser("~/projects")
@@ -28,17 +28,20 @@ def update_projects():
         repo = Repo(project_path)
         if repo.is_dirty():
             print("=========Updating: %s " % (project_path))
-            os.system('''
+            os.system(
+                '''
                 git add -A;
                 git commit -m "commit by schedule";
                 git push origin master
-                ''')
+                '''
+            )
         else:
             print("----------Repository {%s} is clean" % (project_path))
 
-            if __name__ == "__main__":
 
-                # 每隔30分钟调用一次程序更新本地项目文件
+if __name__ == "__main__":
+
+    # 每隔30分钟调用一次程序更新本地项目文件
     schedule.every(1).minutes.do(update_projects)
 
     while True:
