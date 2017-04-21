2e9f197eb984b665d2a4f653a25e14ce0dfdb985
theo-l <theol.liang@gamil.com>
2017-04-21 20:41:21 -0300

    update vagrant alias

---
+++ shell/vagrant_alias.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+alias vagrant_init='vagrant init'
+alias vagrant_up='vagrant up'
+alias vagrant_halt="vagrant halt"
+alias vagrant_reload="vagrant reload"
+alias vagrant_ssh="vagrant ssh"
+alias vagrant_status="vagrant status"
+alias vagrant_destroy="vagrant destroy"
